695fece96c515b63e492ee4b8374732300cc527a
gasparfay <gasparfay@gmail.com>
2025-09-15 17:05:57 -0300

    package-info

---
+++ Entregable1/src/model/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Este paquete contiene las clases principales del modelo de la plataforma de
+ * streaming.
+ * Incluye entidades como Account, AccountManager, Client, NetworkStatus,
+ * Profile y Session.
+ * 
+ * @author Grupo 8
+ * @version 1.0
+ */
+package model;
