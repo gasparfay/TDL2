31d8e69e652601b14cdb838acd6d60d8f706b6d5
gasparfay <gasparfay@gmail.com>
2025-09-15 17:08:30 -0300

    arreglando

--- Entregable1/doc/package-search-index.js
+++ Entregable1/doc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"streamingplatform"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"model"}];updateSearchResults();
