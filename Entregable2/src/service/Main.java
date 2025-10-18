908313f07fdd23e33aaed5e1628f556bf3078125
Gaspar Fay <gasparfay@gmail.com>
2025-10-18 19:34:03 -0300

    aa

--- Entregable2/src/service/Main.java
+++ Entregable2/src/service/Main.java
@@ -1,8 +1,9 @@
 package service;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-	    //Connection con = MyConnection.getConnection(); 
-		//MyConnection.disconnect();
+	    Connection con = MyConnection.getConnection(); 
+		MyConnection.disconnect();
     }
 }   
