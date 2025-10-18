96e944d12e6aaa713fdff7f480a8a17d9f4efbb3
Gaspar Fay <gasparfay@gmail.com>
2025-10-18 19:49:28 -0300

    db

--- Entregable2/src/service/Main.java
+++ Entregable2/src/service/Main.java
@@ -3,7 +3,13 @@ import java.sql.*;
 
 public class Main {
     public static void main(String[] args) {
-	    Connection con = MyConnection.getConnection(); 
-		MyConnection.disconnect();
+	    try {
+            Connection con = MyConnection.getConnection(); 
+            con.createStatement().execute("CREATE TABLE IF NOT EXISTS moneda(nombre TEXT, valor REAL);");
+            con.createStatement().executeUpdate("INSERT INTO moneda(nombre, valor) VALUES('Bitcoin', 67000);");
+		    MyConnection.disconnect();
+        } catch (SQLException e) {  
+            System.out.print("Error en la operación con la BD: " + e.getMessage());
+        }
     }
 }   
