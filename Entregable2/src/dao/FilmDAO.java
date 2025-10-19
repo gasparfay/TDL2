52e8cb7681072231dec68ca59474f74aff10afb9
Gaspar Fay <gasparfay@gmail.com>
2025-10-18 23:35:24 -0300

    ..

--- Entregable2/src/dao/FilmDAO.java
+++ Entregable2/src/dao/FilmDAO.java
@@ -2,10 +2,10 @@ package dao;
 
 import java.sql.SQLException;
 import java.util.List;
-import model.Film;
+import model.*;
 
 
 public interface FilmDAO {
     void loadFilm(Film film) throws SQLException;
-    List<Film> findAll() throws SQLException;
+    List<Film> findAll() throws SQLException;   
 }
