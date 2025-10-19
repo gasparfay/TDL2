31e4e079e263e6435641328b565c47c35b4dc8a4
Gaspar Fay <gasparfay@gmail.com>
2025-10-18 23:37:57 -0300

    dao

--- Entregable2/src/dao/FilmDAO.java
+++ Entregable2/src/dao/FilmDAO.java
@@ -4,8 +4,9 @@ import java.sql.SQLException;
 import java.util.List;
 import model.*;
 
-
 public interface FilmDAO {
+
     void loadFilm(Film film) throws SQLException;
-    List<Film> findAll() throws SQLException;   
+
+    List<Film> findAll() throws SQLException;
 }
--- Entregable2/src/dao/FilmDAOjdbc.java
+++ Entregable2/src/dao/FilmDAOjdbc.java
@@ -1,24 +1,28 @@
 package dao;
 
 import java.sql.Connection;
-
+import java.util.List;
+import model.Film;
 import service.MyConnection;
 
 public class FilmDAOjdbc implements FilmDAO {
-    	private Connection con;
-	
-	    public ActivoDAOjdbc() {
-	    	this.con = MyConnection.getConnection();
-	    }
 
-        @Override
-        public void loadFilm(Film film) {
-            // Implementación del método para cargar una película desde la base de datos
-        }
-        
-        @Override
-        public List<Film> findAll() {
-            // Implementación del método para encontrar todas las películas en la base de datos
-        }
+    private Connection con;
+
+    public FilmDAOjdbc() {
+        this.con = MyConnection.getConnection();
+    }
+
+    @Override
+    public void loadFilm(Film film) {
+        // Implementación del método para cargar una película desde la base de datos
+        return;
+    }
+
+    @Override
+    public List<Film> findAll() {
+        // Implementación del método para encontrar todas las películas en la base de datos
+        return null;
+    }
 
 }
