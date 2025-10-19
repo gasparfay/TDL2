3985f20c84e3388d837e40efd006e8faf7b6a3e8
FrancoGiovagnoli <fgiovagnoli22@alu.ing.unlp.edu.ar>
2025-10-18 23:52:05 -0300

    .

--- Entregable2/src/dao/ReviewDAO.java
+++ Entregable2/src/dao/ReviewDAO.java
@@ -1,5 +1,7 @@
 package dao;
 
-public class ReviewDAO {
-
+public interface ReviewDAO {
+    public void submitReview(int userId, int filmId, String reviewText) throws Exception;
+    public String getReviewStatus(int reviewId) throws Exception;
+    public void updateReviewStatus(int reviewId, String status) throws Exception;
 }
