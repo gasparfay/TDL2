26790c234228ae4e4ad7226c01c2d0d3fc01c9fc
FrancoGiovagnoli <fgiovagnoli22@alu.ing.unlp.edu.ar>
2025-10-18 22:41:52 -0300

    .

--- Entregable2/src/model/Country.java
+++ Entregable2/src/model/Country.java
@@ -1,5 +1,17 @@
 package model;
 
 public enum Country {
-
+    ARGENTINA,
+    BRAZIL,
+    CHILE,
+    PARAGUAY,
+    USA,
+    UK,
+    SPAIN,
+    ITALY,
+    DENMARK,
+    JAPAN,
+    PORTUGAL,
+    CHINA,
+    FRANCE
 }
