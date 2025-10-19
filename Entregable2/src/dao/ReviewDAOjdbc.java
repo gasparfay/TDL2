49af4fcccd7df1d25e50040af333b8bd2c996e97
FrancoGiovagnoli <fgiovagnoli22@alu.ing.unlp.edu.ar>
2025-10-18 23:51:08 -0300

    .

---
+++ Entregable2/src/dao/ReviewDAOjdbc.java
@@ -0,0 +1,32 @@
+package dao;
+import java.sql.Connection;
+
+import service.MyConnection;
+import model.Review;
+
+
+public class ReviewDAOjdbc implements ReviewDAO {
+    private Connection con;
+	
+	public ReviewDAOjdbc() {
+		this.con = MyConnection.getConnection();
+	}
+
+    public void cargarActivo(Review review) {
+		try {
+			String query = "INSERT INTO review(rating,text,creationDate,reviewStatus,lastModified) VALUES(?,?,?,?,?)";
+			PreparedStatement st = con.prepareStatement(query);
+			st.clearParameters();
+			st.setInt(1, review.getRating());
+			st.setString(2, review.getText());
+            st.setDate(3, new java.sql.Date(review.getCreationDate().getTime()));
+            st.setInt(4, review.getStatus());
+            st.setDate(5, new java.sql.Date(review.getLastModified().getTime()));
+			st.executeUpdate();
+            System.out.println("Review guardada correctamente.");
+			st.close();
+		}catch(SQLException e) {
+			System.out.print("Error de SQL: "+e.getMessage());
+		}
+	}
+}
