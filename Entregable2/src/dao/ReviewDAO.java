969677705818008f095193c68d52542145e16404
Gaspar Fay <gasparfay@gmail.com>
2025-10-26 20:57:08 -0300

    .

--- Entregable2/src/dao/ReviewDAO.java
+++ Entregable2/src/dao/ReviewDAO.java
@@ -5,8 +5,6 @@ import model.*;
 
 public interface ReviewDAO {
     void loadReview(Review rev);  
-    List<Review> findRejected();  
-    boolean approve(Review rev);  
-    List<Review> findByFilm(int filmId);
-    List<Review> findByAccount(int accountId);
+    List<Review> findPending();  
+    boolean modifyStatus(Review rev);  
 }
