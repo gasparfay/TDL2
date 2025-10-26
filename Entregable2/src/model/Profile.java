b57677faa442eda5924f98fb65746a7c6f1b411d
Gaspar Fay <gasparfay@gmail.com>
2025-10-26 17:10:59 -0300

    aa

--- Entregable2/src/model/Profile.java
+++ Entregable2/src/model/Profile.java
@@ -3,25 +3,45 @@ package model;
 import java.util.*;
 
 public class Profile {
-
     private int profileId;
+    private Account account;  // Nueva referencia a Account
     private String name;
     private int accountId;
     private List<Review> profileReviews;
 
-	public Profile(String name) {
+    public Profile(String name, Account account) {  // Constructor modificado
+        this.name = name;
+        this.account = account;
+    }
+
+    public int getProfileId() {
+        return profileId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
     }
 
+<<<<<<< Updated upstream
 	public int  getProfileId() {
 		this.accountId=0;
 		return profileId;
 	}
+=======
+    public Account getAccount() {  // Nuevo getter
+        return account;
+    }
+>>>>>>> Stashed changes
 
-	public String getName() {
-		return name;
-	}
+    public void setAccount(Account account) {  // Nuevo setter
+        this.account = account;
+    }
 
+<<<<<<< Updated upstream
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -39,6 +59,9 @@ public class Profile {
 	}
 
 	public List<Review> getProfileReviews() {
+=======
+    public List<Review> getProfileReviews() {
+>>>>>>> Stashed changes
         return profileReviews;
     }
 }
