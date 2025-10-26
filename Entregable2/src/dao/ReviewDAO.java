21a57fb5bcd088a639400d655fcefb4af9160fdc
FrancoGiovagnoli <fgiovagnoli22@alu.ing.unlp.edu.ar>
2025-10-26 18:14:07 -0300

    .

--- Entregable2/src/dao/ReviewDAO.java
+++ Entregable2/src/dao/ReviewDAO.java
@@ -5,9 +5,9 @@ import java.util.List;
 import model.*;
 
 public interface ReviewDAO {
-    void loadReview(Review rev) throws SQLException;  ;
-    List<Review> findPending() throws SQLException;  ;
-    void approve(Review rev) throws SQLException;  ;
+    void loadReview(Review rev) throws SQLException;  
+    List<Review> findRejected() throws SQLException;  
+    boolean approve(Review rev) throws SQLException;  
     List<Review> findByFilm(int filmId) throws SQLException;
     List<Review> findByAccount(int accountId) throws SQLException;
 }
